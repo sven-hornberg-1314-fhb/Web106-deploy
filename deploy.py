15a1f8f78bc3c49f825445082019fc0b49a9c73f
marcel-hinderlich-1314-fhb <marcelh89@googlemail.com>
2014-01-20 18:22:32 +0100

    deploy

--- deploy.py
+++ deploy.py
@@ -1,68 +1,92 @@
-import os,sys,boto
+import os,boto, boto.rds,time,datetime
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
-awsAccessKey = ""
-awsSecretKey = ""
-bucketName = "web106eb"
-fileName = "web106.war"
+class Deploy:
+    awsAccessKey = ""
+    awsSecretKey = ""
+    bucketName = "web106eb"
+    fileName = "web106.war"
+    dbUser = "web106db"
+    dbUserPassword = "web106db"
+    dbEndpoint = None
+    bucketKey = None
 
-def main():
-    print('Los')
-    changeProjectDir()
-
-    #createWarFile()
-    #uploadWarFile()
-    createDbRds()
-
-def checkoutProjectFromGit():
-    print "checkout"
-
-def changeProjectDir():
-    os.chdir('../Web106')
-    print(os.getcwd())
-
-def createWarFile():
-    print 'Try to create war file...'
-    os.system("./grailsw prod war " + str(fileName))
-    
-def uploadWarFile():
-    print "start uploading war file"
-    connS3 = S3Connection(awsAccessKey, awsSecretKey) 
-    nonExistend = connS3.lookup(bucketName)
-    bucket = None
-    
-    if nonExistend is None:
-        print "start creating bucket"
-        bucket = connS3.create_bucket(bucketName)
-        print "done creating bucket"
-    else:
-        bucket = connS3.get_bucket(bucketName)
-        print "bucket already exists"
+    def __init__(self):
+        date = datetime.datetime.now()
+        print('starting deployment at '+str(date.hour)+":"+str(date.minute))
+        self.changeProjectDir()
         
     
-    k = Key(bucket)
-    k.key = fileName
-    print "uploading..."
-    k.set_contents_from_filename(fileName)
-    print "done uploading war file"
+        #self.createWarFile()
+        #self.uploadWarFile()
+        self.createDbRds()
+        print self.dbEndpoint
+        print self.bucketKey
     
-def createDbRds():
-     = boto.rds.connect_to_region(
-        "eu-west-1",
-        awsAccessKey,
-        awsSecretKey
-    )
-    connRds = boto.rds.connect_to_region()
-
+    def checkoutProjectFromGit(self):
+        print "checkout"
     
-    db = connRds.create_dbinstance("dbeb",5,"t1.micro","web106db","web106db")
+    def changeProjectDir(self):
+        os.chdir('../Web106')
+        print(os.getcwd())
     
-    
+    def createWarFile(self):
+        print 'Try to create war file...'
+        os.system("./grailsw prod war " + str(self.fileName))
         
+    def uploadWarFile(self):
+        print "start uploading war file"
+        connS3 = S3Connection(self.awsAccessKey, self.awsSecretKey) 
+        nonExistend = connS3.lookup(self.bucketName)
+        bucket = None
+        
+        if nonExistend is None:
+            print "start creating bucket"
+            bucket = connS3.create_bucket(self.bucketName)
+            print "done creating bucket"
+        else:
+            bucket = connS3.get_bucket(self.bucketName)
+            print "bucket already exists"
+            
+        
+        k = Key(bucket)
+        k.key = self.fileName
+        print "uploading..."
+        k.set_contents_from_filename(self.fileName)
+        
+        self.bucketKey = k.key
+        
+        print "done uploading war file"
+        
+    def createDbRds(self):
+        connRds = boto.rds.connect_to_region(
+            "eu-west-1",
+            aws_access_key_id=self.awsAccessKey, 
+            aws_secret_access_key=self.awsSecretKey
+        )
+        db = None
+    
+        check = connRds.get_all_dbinstances("dbeb")
+        if(len(check) == 0):
+            db = connRds.create_dbinstance("dbeb",5,"db.t1.micro",self.dbUser,self.dbUserPassword) 
+    
+        instances = connRds.get_all_dbinstances("dbeb")
+        db = instances[0]
+
+        while db.endpoint == None:
+            time.sleep(5)
+            date = datetime.datetime.now()
+            print "waiting for endpoint "+str(date.hour)+":"+str(date.minute)
+         
+        self.dbEndpoint = db.endpoint[0]
+
+
+
 
 if __name__ == "__main__":
-    main()
+    d = Deploy()
+    
 
 
 
